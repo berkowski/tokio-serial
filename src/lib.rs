aa314c9e3840b1b40c03e4fea88790b634ca43fe
Laurentiu Nicola <lnicola@dend.ro>
2018-04-14 10:48:30 +0300

    Remove SerialPort docs, as they're available on the original trait

--- src/lib.rs
+++ src/lib.rs
@@ -112,221 +112,90 @@ impl Serial {
 }
 
 impl ::SerialPort for Serial {
-    /// Returns a struct with the current port settings
     fn settings(&self) -> ::SerialPortSettings {
         self.io.get_ref().settings()
     }
 
-    /// Return the name associated with the serial port, if known.
     fn port_name(&self) -> Option<String> {
         self.io.get_ref().port_name()
     }
 
-    /// Returns the current baud rate.
-    ///
-    /// This function returns `None` if the baud rate could not be determined. This may occur if
-    /// the hardware is in an uninitialized state. Setting a baud rate with `set_baud_rate()`
-    /// should initialize the baud rate to a supported value.
     fn baud_rate(&self) -> Option<::BaudRate> {
         self.io.get_ref().baud_rate()
     }
 
-    /// Returns the character size.
-    ///
-    /// This function returns `None` if the character size could not be determined. This may occur
-    /// if the hardware is in an uninitialized state or is using a non-standard character size.
-    /// Setting a baud rate with `set_char_size()` should initialize the character size to a
-    /// supported value.
     fn data_bits(&self) -> Option<::DataBits> {
         self.io.get_ref().data_bits()
     }
 
-    /// Returns the flow control mode.
-    ///
-    /// This function returns `None` if the flow control mode could not be determined. This may
-    /// occur if the hardware is in an uninitialized state or is using an unsupported flow control
-    /// mode. Setting a flow control mode with `set_flow_control()` should initialize the flow
-    /// control mode to a supported value.
     fn flow_control(&self) -> Option<::FlowControl> {
         self.io.get_ref().flow_control()
     }
 
-    /// Returns the parity-checking mode.
-    ///
-    /// This function returns `None` if the parity mode could not be determined. This may occur if
-    /// the hardware is in an uninitialized state or is using a non-standard parity mode. Setting
-    /// a parity mode with `set_parity()` should initialize the parity mode to a supported value.
     fn parity(&self) -> Option<::Parity> {
         self.io.get_ref().parity()
     }
 
-    /// Returns the number of stop bits.
-    ///
-    /// This function returns `None` if the number of stop bits could not be determined. This may
-    /// occur if the hardware is in an uninitialized state or is using an unsupported stop bit
-    /// configuration. Setting the number of stop bits with `set_stop-bits()` should initialize the
-    /// stop bits to a supported value.
     fn stop_bits(&self) -> Option<::StopBits> {
         self.io.get_ref().stop_bits()
     }
 
-    /// Returns the current timeout.
     fn timeout(&self) -> Duration {
         Duration::from_secs(0)
     }
 
-    // Port settings setters
-
-    /// Applies all settings for a struct. This isn't guaranteed to involve only
-    /// a single call into the driver, though that may be done on some
-    /// platforms.
     fn set_all(&mut self, settings: &::SerialPortSettings) -> ::Result<()> {
         self.io.get_mut().set_all(settings)
     }
 
-    /// Sets the baud rate.
-    ///
-    /// ## Errors
-    ///
-    /// If the implementation does not support the requested baud rate, this function may return an
-    /// `InvalidInput` error. Even if the baud rate is accepted by `set_baud_rate()`, it may not be
-    /// supported by the underlying hardware.
     fn set_baud_rate(&mut self, baud_rate: ::BaudRate) -> ::Result<()> {
         self.io.get_mut().set_baud_rate(baud_rate)
     }
 
-    /// Sets the character size.
     fn set_data_bits(&mut self, data_bits: ::DataBits) -> ::Result<()> {
         self.io.get_mut().set_data_bits(data_bits)
     }
 
-    /// Sets the flow control mode.
     fn set_flow_control(&mut self, flow_control: ::FlowControl) -> ::Result<()> {
         self.io.get_mut().set_flow_control(flow_control)
     }
 
-    /// Sets the parity-checking mode.
     fn set_parity(&mut self, parity: ::Parity) -> ::Result<()> {
         self.io.get_mut().set_parity(parity)
     }
 
-    /// Sets the number of stop bits.
     fn set_stop_bits(&mut self, stop_bits: ::StopBits) -> ::Result<()> {
         self.io.get_mut().set_stop_bits(stop_bits)
     }
 
-    /// Sets the timeout for future I/O operations.  This parameter is ignored but
-    /// required for trait completeness.
     fn set_timeout(&mut self, _: Duration) -> ::Result<()> {
         Ok(())
     }
 
-    // Functions for setting non-data control signal pins
-
-    /// Sets the state of the RTS (Request To Send) control signal.
-    ///
-    /// Setting a value of `true` asserts the RTS control signal. `false` clears the signal.
-    ///
-    /// ## Errors
-    ///
-    /// This function returns an error if the RTS control signal could not be set to the desired
-    /// state on the underlying hardware:
-    ///
-    /// * `NoDevice` if the device was disconnected.
-    /// * `Io` for any other type of I/O error.
     fn write_request_to_send(&mut self, level: bool) -> ::Result<()> {
         self.io.get_mut().write_request_to_send(level)
     }
 
-    /// Writes to the Data Terminal Ready pin
-    ///
-    /// Setting a value of `true` asserts the DTR control signal. `false` clears the signal.
-    ///
-    /// ## Errors
-    ///
-    /// This function returns an error if the DTR control signal could not be set to the desired
-    /// state on the underlying hardware:
-    ///
-    /// * `NoDevice` if the device was disconnected.
-    /// * `Io` for any other type of I/O error.
     fn write_data_terminal_ready(&mut self, level: bool) -> ::Result<()> {
         self.io.get_mut().write_data_terminal_ready(level)
     }
 
-    // Functions for reading additional pins
-
-    /// Reads the state of the CTS (Clear To Send) control signal.
-    ///
-    /// This function returns a boolean that indicates whether the CTS control signal is asserted.
-    ///
-    /// ## Errors
-    ///
-    /// This function returns an error if the state of the CTS control signal could not be read
-    /// from the underlying hardware:
-    ///
-    /// * `NoDevice` if the device was disconnected.
-    /// * `Io` for any other type of I/O error.
     fn read_clear_to_send(&mut self) -> ::Result<bool> {
         self.io.get_mut().read_clear_to_send()
     }
 
-    /// Reads the state of the Data Set Ready control signal.
-    ///
-    /// This function returns a boolean that indicates whether the DSR control signal is asserted.
-    ///
-    /// ## Errors
-    ///
-    /// This function returns an error if the state of the DSR control signal could not be read
-    /// from the underlying hardware:
-    ///
-    /// * `NoDevice` if the device was disconnected.
-    /// * `Io` for any other type of I/O error.
     fn read_data_set_ready(&mut self) -> ::Result<bool> {
         self.io.get_mut().read_data_set_ready()
     }
 
-    /// Reads the state of the Ring Indicator control signal.
-    ///
-    /// This function returns a boolean that indicates whether the RI control signal is asserted.
-    ///
-    /// ## Errors
-    ///
-    /// This function returns an error if the state of the RI control signal could not be read from
-    /// the underlying hardware:
-    ///
-    /// * `NoDevice` if the device was disconnected.
-    /// * `Io` for any other type of I/O error.
     fn read_ring_indicator(&mut self) -> ::Result<bool> {
         self.io.get_mut().read_ring_indicator()
     }
 
-    /// Reads the state of the Carrier Detect control signal.
-    ///
-    /// This function returns a boolean that indicates whether the CD control signal is asserted.
-    ///
-    /// ## Errors
-    ///
-    /// This function returns an error if the state of the CD control signal could not be read from
-    /// the underlying hardware:
-    ///
-    /// * `NoDevice` if the device was disconnected.
-    /// * `Io` for any other type of I/O error.
     fn read_carrier_detect(&mut self) -> ::Result<bool> {
         self.io.get_mut().read_carrier_detect()
     }
 
-    /// Attempts to clone the `SerialPort`. This allow you to write and read simultaneously from the
-    /// same serial connection. Please note that if you want a real asynchronous serial port you
-    /// should look at [mio-serial](https://crates.io/crates/mio-serial) or
-    /// [tokio-serial](https://crates.io/crates/tokio-serial).
-    ///
-    /// Also, you must be very carefull when changing the settings of a cloned `SerialPort` : since
-    /// the settings are cached on a per object basis, trying to modify them from two different
-    /// objects can cause some nasty behavior.
-    ///
-    /// # Errors
-    ///
-    /// This function returns an error if the serial port couldn't be cloned.
     fn try_clone(&self) -> ::Result<Box<::SerialPort>> {
         self.io.get_ref().try_clone()
     }
